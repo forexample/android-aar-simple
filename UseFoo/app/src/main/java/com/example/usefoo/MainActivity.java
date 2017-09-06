65d6598c893c85032b8ea5a6c8e700099100127a
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-09-07 00:17:28 +0300

    Use library (add 'import com.example.foo')

--- UseFoo/app/src/main/java/com/example/usefoo/MainActivity.java
+++ UseFoo/app/src/main/java/com/example/usefoo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.usefoo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.foo;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
