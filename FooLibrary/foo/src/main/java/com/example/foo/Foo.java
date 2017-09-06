5a7dbeb24621c62e6b1acff11c4c00f5b978f865
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-09-06 23:53:04 +0300

    Add FooLibrary example

---
+++ FooLibrary/foo/src/main/java/com/example/foo/Foo.java
@@ -0,0 +1,14 @@
+package com.example.foo;
+
+/**
+ * Created by ruslo on 06/09/17.
+ */
+
+public class Foo {
+    public Foo() {
+    }
+
+    public int getResult() {
+        return 42;
+    }
+}
