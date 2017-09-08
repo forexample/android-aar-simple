0e080e46d2c5b1df21a4cd2fc1bdc73bd3ec7f98
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-09-08 13:26:09 +0300

    Add C++ part

---
+++ FooLibraryCpp/foo.cpp
@@ -0,0 +1,15 @@
+#include <jni.h>
+
+int foo() {
+#if defined(__aarch64__)
+  return 13;
+#else
+  return 42;
+#endif
+}
+
+JNIEXPORT jint JNICALL Java_com_example_foo_Foo_getResult(
+    JNIEnv* env, jobject thiz
+) {
+  return foo();
+}
